02588b1cbae1cd5de856b81e757f0b3a73f0214d
WKHAllen <wkhallen@gmail.com>
2021-03-23 00:24:36 -0500

    Add support for static files

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const path = require("path");
 const port = parseInt(process.env.PORT);
 
 const pagesDir = "pages";
+const staticDir = "static";
 const pages = ["error", "maintenance"];
 
-function sendFile(res, urlPath, callback) {
+function sendHTML(res, urlPath, callback) {
   fs.readFile(
     path.join(__dirname, pagesDir, urlPath + ".html"),
     (err, data) => {
       if (err) {
         callback(err);
       } else {
+        res.writeHeader(200, { "Content-Type": "text/html" });
         res.write(data, (writeErr) => {
           if (writeErr) {
             callback(writeErr);
@@ -26,11 +28,37 @@ function sendFile(res, urlPath, callback) {
   );
 }
 
+function sendStatic(res, urlPath, callback) {
+  fs.readFile(path.join(__dirname, staticDir, urlPath), (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      res.write(data, (writeErr) => {
+        if (writeErr) {
+          callback(writeErr);
+        } else {
+          callback(null);
+        }
+      });
+    }
+  });
+}
+
 const server = http.createServer((req, res) => {
   const urlPath = req.url.slice(1);
 
   if (pages.includes(urlPath)) {
-    sendFile(res, urlPath, (err) => {
+    sendHTML(res, urlPath, (err) => {
+      if (err) {
+        console.error(err);
+      }
+      res.end();
+    });
+  } else if (
+    fs.existsSync(path.join(__dirname, staticDir, urlPath)) &&
+    fs.lstatSync(path.join(__dirname, staticDir, urlPath)).isFile()
+  ) {
+    sendStatic(res, urlPath, (err) => {
       if (err) {
         console.error(err);
       }
